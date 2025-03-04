830faf543c196b9520b509b5be30e2d45be1b56d
Michael Fernando Hernandez Peralta <maicolhernandez420@gmail.com>
2025-03-04 11:15:59 -0500

    Removes redundant database config
    Removes hardcoded database configuration values.  The database connection string is now constructed using values from the application's settings.

--- app/core/database.py
+++ app/core/database.py
@@ -1,12 +1,6 @@
 from sqlmodel import SQLModel, create_engine, Session
 from app.core.config import settings
 
-# DB_NAME = os.getenv("DB_NAME", "test_db")
-# print(f"Hello {DB_NAME} from Python")
-# DB_USER = os.getenv("DB_USER", "app_user")
-# DB_PASSWORD = os.getenv("DB_PASSWORD", "secret123")
-# DB_HOST = os.getenv("DB_HOST", "mysql")
-# DB_PORT = os.getenv("DB_PORT", "3306")
 
 # Use asyncmy for asynchronous operations
 DATABASE_URL = f"mysql+mysqldb://{settings.db_user}:{settings.db_password}@{settings.db_host}:{settings.db_port}/{settings.db_name}"
