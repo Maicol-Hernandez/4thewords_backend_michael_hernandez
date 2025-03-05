45cdede59db0eabdb358067f415365f3e2c42e6c
Michael Fernando Hernandez Peralta <maicolhernandez420@gmail.com>
2025-03-05 12:23:36 -0500

    Adds CORS middleware
    Enables Cross-Origin Resource Sharing (CORS) to allow requests from any origin.  This addresses potential issues with front-end applications making requests to the API.

--- app/main.py
+++ app/main.py
@@ -1,9 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 from app.core.database import create_db_and_tables
 from app.api.v1.endpoints import legends
 
 app = FastAPI()
+
+# CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Mounts the "app/static" directory at the "/static" path
 app.mount("/static", StaticFiles(directory="app/static"), name="static")
