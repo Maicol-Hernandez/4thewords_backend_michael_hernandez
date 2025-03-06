4e5a6c752053c4763a1dfcc34fb57e2b3358a6e5
Michael Fernando Hernandez Peralta <maicolhernandez420@gmail.com>
2025-03-06 05:25:07 -0500

    Adds image URL prefixing
    Adds a validator to legend schemas to automatically prefix image URLs with "http://localhost:8080".

--- app/schemas/legend.py
+++ app/schemas/legend.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, validator
 from typing import Optional
 from datetime import datetime, date
 
@@ -11,11 +11,15 @@ class LegendCreate(BaseModel):
     province: str
     canton: str
     district: str
-    
+
+
 class LegendResponse(LegendCreate):
     id: int
     image_url: str
     created_at: datetime
     updated_at: Optional[datetime]
     # deleted_at: Optional[datetime]
-    
+
+    @validator("image_url", pre=True)
+    def add_prefix(cls, v):
+        return f"http://localhost:8080{v}"
